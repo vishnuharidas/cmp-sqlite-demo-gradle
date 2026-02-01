497785c6b353198a3420b35bca5c9a54d62e2225
Vishnu Haridas <hvishnu999@gmail.com>
2026-02-01 20:01:22 +0400

    Add database factories in iOS and Android

--- composeApp/src/androidMain/kotlin/com/example/cmp_sqlite_demo/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/example/cmp_sqlite_demo/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.cmp_sqlite_demo.data.getDatabaseBuilder
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,13 +14,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+            App(
+                getDatabaseBuilder(application)
+                    .build()
+            )
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
--- composeApp/src/iosMain/kotlin/com/example/cmp_sqlite_demo/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/example/cmp_sqlite_demo/MainViewController.kt
@@ -1,5 +1,8 @@
 package com.example.cmp_sqlite_demo
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.example.cmp_sqlite_demo.data.getDatabaseBuilder
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    App(getDatabaseBuilder().build())
+}
